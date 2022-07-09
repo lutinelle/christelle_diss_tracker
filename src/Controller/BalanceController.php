47bd2542b6cdbe65a1918ae2f4074adbc01c6a9c
christelle diss <lutinelle01@yahoo.fr>
2022-07-09 16:44:21 +0200

    initiate graph

--- src/Controller/BalanceController.php
+++ src/Controller/BalanceController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class BalanceController extends AbstractController
 {
     #[Route('/graph', name: 'app_balance')]
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([/* ... */]);
+
         return $this->render('balance/index.html.twig', [
-            'controller_name' => 'BalanceController',
+            'chart' => $chart,
         ]);
     }
 }
